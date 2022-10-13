1f59dd03d1345cb1e9c32eb48d3c39731b11a605
xinnj <xinnj@hotmail.com>
2022-10-13 09:10:37 +0800

    Add workflow

--- installer/build.ps1
+++ installer/build.ps1
@@ -7,6 +7,9 @@ param (
 New-Item -ItemType Directory -Force -Path .\build
 
 $VersionNum = $Version -replace ".*?(\d.*)", "`${1}"
+Write-Host "Version: ${Version}"
+Write-Host "VersionNum: ${VersionNum}"
+
 ((Get-Content .\installer\config.yaml -Raw -Encoding utf8) -replace "(Version:) .*", "`${1} '${VersionNum}'") |
         Set-Content .\installer\config.yaml -Encoding utf8
 .\source\wixc.ps1 -config .\installer\config.yaml -output .\build\WiXCover-${Version}.msi
